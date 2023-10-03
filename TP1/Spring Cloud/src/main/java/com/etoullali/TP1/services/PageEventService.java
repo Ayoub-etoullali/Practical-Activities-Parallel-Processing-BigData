b93580164e4e35c5193e72a8653bf7b187143cd8
Ayoub ETOULLALI <ayoub.etoullali2002@gmail.com>
2023-10-04 00:24:32 +0100

    TP1

--- TP1/Spring Cloud/src/main/java/com/etoullali/TP1/services/PageEventService.java
+++ TP1/Spring Cloud/src/main/java/com/etoullali/TP1/services/PageEventService.java
@@ -1,9 +1,16 @@
 package com.etoullali.TP1.services;
 
 import com.etoullali.TP1.entities.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -14,28 +21,85 @@ import java.util.function.Supplier;
 public class PageEventService {
 
     @Bean
-    public Consumer<PageEvent> pageEventConsumer(){
-        return (input)->{
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input) -> {
             System.out.println("**********************");
             System.out.println(input.toString());
         };
     }
 
     @Bean
-    public Supplier<PageEvent> pageEventSupplier(){
-        return ()-> new PageEvent(
-                    Math.random()>0.5?"P1":"P2",
-                    Math.random()>0.5?"U1":"U2",
-                    new Date(),
-                    new Random().nextInt(9000)
-            );
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> new PageEvent(
+                Math.random() > 0.5 ? "P1" : "P2",
+                Math.random() > 0.5 ? "U1" : "U2",
+                new Date(),
+                new Random().nextInt(9000)
+        );
     }
 
     @Bean
-    public Function<PageEvent,PageEvent> pageEventFunction(){
-        return (input)->{
-            input.setName("this name: "+input.getName()+" is well received");
+    public Function<PageEvent, PageEvent> pageEventFunction() {
+        return (input) -> {
+            input.setName("this name: " + input.getName() + " is well received");
             return input;
         };
     }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction() {
+        return (input) -> {
+            return input
+
+                    .filter((k, v) -> v.getDuration() > 100)
+                    .map((k, v) -> new KeyValue<>(v.getName(), v.getDuration()))
+                    .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(1)))
+                    .reduce((acc, v) -> {
+                            long sum = (acc + v) / 2;
+                            return sum;
+                        },
+                            Materialized.as("stats-store")
+                    )
+                    .toStream()
+                    .map((k, v) -> new KeyValue<>("=>" + k.window().startTime() + k.window().endTime() + ":" + k.key(), v));
+
+        };
+    }
 }
+    /*
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction() {
+        System.out.println("test");
+        return (input) -> input
+                .filter(
+                        (k, v) -> v.getDuration() > 100
+                )
+                .map(
+                        (k, v) -> new KeyValue<>(v.getName(), 0L)
+                )
+                .groupBy(
+                        (k, v) -> k, Grouped.with(Serdes.String(), Serdes.Long())
+                )
+//                .groupByKey(
+//                        Grouped.keySerde(Serdes.String())
+//                )
+                .windowedBy(
+                        TimeWindows.of(Duration.ofSeconds(1))
+                ) //return un objet de type K-Table
+                .count(
+                        Materialized.as("page-count")
+                )
+                .toStream() //convertir to K-Stream (K-Table)
+                .map(
+                    (k, v) -> new KeyValue<>(""+k.window().startTime()+k.window().endTime()+k.key(), v)
+                );
+    }
+}
+*/
+
+/*
+=> [N.B]
+    - winddowedBy:  donne la résultat récente sans le comulée (l'évolution)
+    - Materialized: view matérialisé
+ */
--- TP1/Spring Cloud/src/main/java/com/etoullali/TP1/web/PageEventRestController.java
+++ TP1/Spring Cloud/src/main/java/com/etoullali/TP1/web/PageEventRestController.java
@@ -1,13 +1,25 @@
 package com.etoullali.TP1.web;
 
 import com.etoullali.TP1.entities.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 //=> La visite de la page
@@ -16,19 +28,42 @@ import java.util.Random;
 public class PageEventRestController {
     @Autowired
     private StreamBridge streamBridge;
+
     @GetMapping("/publish/{topic}/{name}") //publier un évènement dans un topic Kafka
-    public PageEvent publish(@PathVariable String topic, @PathVariable String name){
-        PageEvent pageEvent=new PageEvent(name,Math.random()>0.5?"U1":"U2",new Date(),new Random().nextInt(9000));
-        streamBridge.send(topic,pageEvent);
+    public PageEvent publish(@PathVariable String topic, @PathVariable String name) {
+        PageEvent pageEvent = new PageEvent(name, Math.random() > 0.5 ? "U1" : "U2", new Date(), new Random().nextInt(9000));
+        streamBridge.send(topic, pageEvent);
         return pageEvent;
     }
-}
 
-/*
-=> [N.B]
-    - Supplier: permet de créer un poller qui return un message qui va etre dans le topic Kafka (timer) [ex: capteur]
-    - Poller: un produit qui va produire un message chaque seconde par exemple
-    - Functions: consommer + triter + publier le résultat dans un autre tipic
-    - Kafka streams: pour le data analysis (visualiser en temps réel)
-    - StreamBridge: s'avantage peut fonctionner avec JMS, RabbitMQ, Kafka (pour KafkaTemplate ça marche juste pour Kafka)
- */
+    /*
+    => [N.B]
+        - Supplier: permet de créer un poller qui return un message qui va etre dans le topic Kafka (timer) [ex: capteur]
+        - Poller: un produit qui va produire un message chaque seconde par exemple
+        - Functions: consommer + triter + publier le résultat dans un autre tipic
+        - Kafka streams: pour le data analysis (visualiser en temps réel)
+        - StreamBridge: s'avantage peut fonctionner avec JMS, RabbitMQ, Kafka (pour KafkaTemplate ça marche juste pour Kafka)
+     */
+
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
+
+    @GetMapping(path = "/analytics", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String, Long>> analytics() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> {
+                    Map<String, Long> stringLongMap = new HashMap<>();
+                    ReadOnlyWindowStore<String, Long> stats =
+                            interactiveQueryService.getQueryableStore("stats-store", QueryableStoreTypes.windowStore());
+                    Instant now = Instant.now();
+                    Instant from = now.minusMillis(5000);
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = stats.fetchAll(from, now);
+                    //WindowStoreIterator<Long> fetch = stats.fetch(name, from, now);
+                    while (fetchAll.hasNext()) {
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(), next.value);
+                    }
+                    return stringLongMap;
+                }).share();
+    }
+}
