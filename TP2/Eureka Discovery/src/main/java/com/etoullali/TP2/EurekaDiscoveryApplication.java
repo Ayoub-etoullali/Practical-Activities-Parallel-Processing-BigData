e6fb1f1e3dcea921a1342dcf3a5b74cc3d1fa02d
Ayoub ETOULLALI <ayoub.etoullali2002@gmail.com>
2023-10-04 18:36:37 +0100

    TP2

--- TP2/Eureka Discovery/src/main/java/com/etoullali/TP2/EurekaDiscoveryApplication.java
+++ TP2/Eureka Discovery/src/main/java/com/etoullali/TP2/EurekaDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.etoullali.TP2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //Activer Eureka Server
 public class EurekaDiscoveryApplication {
 
     public static void main(String[] args) {
--- TP2/Gateway/src/main/java/com/etoullali/TP2/GatewayApplication.java
+++ TP2/Gateway/src/main/java/com/etoullali/TP2/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.etoullali.TP2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +16,27 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    //configurer les rootes
-    RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+    //=>Méthode statique: je connais les routes
+    //@Bean
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder){ //configurer les rootes
         return builder.routes()
-                .route(r->r.path("/customers/**").uri("http://localhost:8081/"))
-                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
+//                .route(r->r.path("/customers/**").uri("http://localhost:8081/"))
+//                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
+                .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r->r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
+
+    }
+
+    //=>Méthode dynamique:
+    //  - je ne connais pas les route
+    //  - à chaque fois que tu reçoit une requete, regarde dans l'URL de la requete tu vas trouvé le nom du Micro Service
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(
+                                                ReactiveDiscoveryClient rdc,
+                                                DiscoveryLocatorProperties dlp){
+
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 
     }
 }
