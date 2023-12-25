1058c899da34d31e1b3b5af6caa3d8da42b11107
Ayoub ETOULLALI <ayoub.etoullali2002@gmail.com>
2023-12-25 12:18:29 +0100

    examen finale

---
+++ Examen/config-consul/src/main/java/org/sid/billing/ConsulApplication.java
@@ -0,0 +1,31 @@
+package org.sid.billing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultTemplate;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class ConsulApplication {
+	@Autowired
+	private VaultTemplate vaultTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args -> {
+			//Ajouter un secret <keypair>
+			vaultTemplate.opsForVersionedKeyValue("secret")
+					.put("keypair",
+							Map.of("privKey","54321",
+									"pubKey","8999"));
+		};
+	};
+
+}
---
+++ Examen/config-consul/src/test/java/org/sid/billing/ConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
