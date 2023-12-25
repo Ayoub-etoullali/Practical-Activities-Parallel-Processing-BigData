4e95b7ae27a41e8b994f23824edbcdcc61b2525d
Ayoub ETOULLALI <ayoub.etoullali2002@gmail.com>
2023-12-25 12:24:14 +0100

    examen finale

---
+++ Examen/README.md
@@ -0,0 +1,6 @@
+<br>
+
+![ayoub](https://user-images.githubusercontent.com/92756846/220727344-dbb21e84-4584-4055-bde5-a3c90a64a618.jpg)
+
+<kbd>Enjoy Code</kbd> 👨‍💻
+[My portfolio](https://ayoub-etoullali.netlify.app/)
---
+++ Examen/config-consul/src/main/java/org/sid/billing/ConsulConfigRestController.java
@@ -0,0 +1,39 @@
+package org.sid.billing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ConsulConfigRestController {
+
+    //=> with dependency Vault
+    @Autowired
+    private MyConsulConfig myConsulConfig;
+    @Autowired
+    private MyVaultConfig myVaultConfig;
+
+    //=> without dependency Vault
+    /*
+    @Value("${token.accessTokenTimeout}")
+    private long accessTokenTimeout;
+    @Value("${token.refreshTokenTimeout}")
+    private long refreshTokenTimeout;
+     */
+
+    @GetMapping("/myConfig")
+    public Map<String,Object> myConfig(){
+        return Map.of("consulConfig",myConsulConfig, "vaultConfig",myVaultConfig);
+    }
+
+    //=> other Methods
+    /*
+    public MyConsulConfig myConfig(){
+        return myConsulConfig;
+    }
+     */
+}
